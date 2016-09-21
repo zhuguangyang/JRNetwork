d9108651b7e6be0184f6c9588235ea5104073a1f
wangxiaotao <445242970@qq.com>
2016-09-21 17:02:00 +0800

    改回来

--- JRNetwork/Classes/Serializable+Extension.swift
+++ JRNetwork/Classes/Serializable+Extension.swift
@@ -35,9 +35,6 @@ extension String: ResponseObjectSerializable {
     }
 }
 
-
-
-
 extension String: ResponseCollectionSerializable {}
 
 
--- JRNetwork/Classes/Serializable.swift
+++ JRNetwork/Classes/Serializable.swift
@@ -14,21 +14,20 @@ public protocol ResponseCollectionSerializable {
     static func collection(from response: NSHTTPURLResponse, withRepresentation representation: AnyObject) -> [Self]?
 }
 
-// 不能做以下这个假设
-//public extension ResponseCollectionSerializable where Self: ResponseObjectSerializable {
-//    
-//    static func collection(from response: NSHTTPURLResponse, withRepresentation representation: AnyObject) -> [Self]? {
-//    
-//        guard let representation = representation as? [AnyObject] else {
-//            return nil
-//        }
-//        
-//        typealias T = Self
-//        
-//        return representation.flatMap { item -> T? in
-//            return T(from: response, withRepresentation: item)
-//        }
-//    }
-//}
+public extension ResponseCollectionSerializable where Self: ResponseObjectSerializable {
+    
+    static func collection(from response: NSHTTPURLResponse, withRepresentation representation: AnyObject) -> [Self]? {
+    
+        guard let representation = representation as? [AnyObject] else {
+            return nil
+        }
+        
+        typealias T = Self
+        
+        return representation.flatMap { item -> T? in
+            return T(from: response, withRepresentation: item)
+        }
+    }
+}
 
 
